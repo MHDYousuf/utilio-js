f566731f38580c217b3755496ad34413b2053253
MHDYousuf <mohd4yousuf@gmail.com>
2023-07-22 17:11:51 +0530

    compileTime added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import sumArray from './sumArray.js'
+import isAnagram from './isAnagram.js'
+import twoSum from './twoSum.js'
+import compileTime from './compileTime.js'
 
-export { sumArray };
+export { sumArray,isAnagram,twoSum,compileTime };
